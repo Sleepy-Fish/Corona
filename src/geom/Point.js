37b0e2898819142d4f938fe50a4b32bbfe5f80c6
J Riley Harrington <riley@sleepy.fish>
2019-12-26 17:40:32 -0500

    Point functions

--- src/geom/Point.js
+++ src/geom/Point.js
@@ -7,4 +7,24 @@ export default class Point {
   static Zero () {
     return new Point(0, 0);
   }
+
+  // ** --- Point Geometry Math Functions --- ** //
+  distance (point) {
+    const dx = point.x - this.x;
+    const dy = point.y = this.y;
+    return Math.sqrt((dx * dx) + (dy * dy));
+  }
+
+  // ** --- Point Utility Functions --- ** //
+  toString () {
+    return `Point[${this.x}, ${this.y}]`;
+  }
+
+  json () {
+    return { x: this.x, y: this.y };
+  }
+
+  copy () {
+    return new Point(this.x, this.y);
+  }
 }
